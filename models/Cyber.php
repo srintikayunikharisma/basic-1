fdbac901837a4cbfb15592fa95d1ec2c90b446a5
AfifahtulHidayatiZ <deazulva58@gmail.com>
2021-06-07 21:58:47 +0700

    commit pertama

---
+++ models/Cyber.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "cyber".
+ *
+ * @property int $id
+ * @property string $nama
+ * @property string $tgl_lahir
+ * @property string $jekel
+ * @property int $angkatan
+ * @property string $divisi
+ */
+class Cyber extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'cyber';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nama', 'tgl_lahir', 'jekel', 'angkatan', 'divisi'], 'required'],
+            [['tgl_lahir'], 'safe'],
+            [['angkatan'], 'integer'],
+            [['nama'], 'string', 'max' => 50],
+            [['jekel'], 'string', 'max' => 5],
+            [['divisi'], 'string', 'max' => 20],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nama' => 'Nama',
+            'tgl_lahir' => 'Tgl Lahir',
+            'jekel' => 'Jekel',
+            'angkatan' => 'Angkatan',
+            'divisi' => 'Divisi',
+        ];
+    }
+}
